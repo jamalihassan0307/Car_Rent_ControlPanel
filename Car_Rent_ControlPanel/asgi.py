de3f40fe325ba928554cddaf590e5f272a9c4aa9
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-17 12:53:30 +0500

    first commit

---
+++ Car_Rent_ControlPanel/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Car_Rent_ControlPanel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Car_Rent_ControlPanel.settings')
+
+application = get_asgi_application()
